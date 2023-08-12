ca21ad982721adc11b672ad7437e2dd5e737e971
Oscar González Tur <44144257+Krontur@users.noreply.github.com>
2023-08-12 14:17:14 +0200

    Second update with create product function.

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAZIdYfRqTIXuF7b2vnldGsSLugS595-S4",
+  authDomain: "warehouse-9659a.firebaseapp.com",
+  projectId: "warehouse-9659a",
+  storageBucket: "warehouse-9659a.appspot.com",
+  messagingSenderId: "1008749761087",
+  appId: "1:1008749761087:web:18639ef5eef766d40d9e8a",
+  measurementId: "G-3Y9RWX8TF1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
