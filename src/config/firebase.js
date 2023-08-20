e541c5034a5dc950bedb151805037968338142df
Oscar González Tur <44144257+Krontur@users.noreply.github.com>
2023-08-20 18:16:05 +0200

    Masking API key

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,6 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId
 };
 
 // Initialize Firebase
