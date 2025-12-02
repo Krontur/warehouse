f3356ca01d76d93cdf3a3f1efadb37ec52c35448
Oscar González Tur <44144257+Krontur@users.noreply.github.com>
2025-12-02 12:04:42 +0100

    Remove Firebase API keys and replace with env variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration
 const firebaseConfig = {
-    apiKey: "***REMOVED***",
-    authDomain: "warehouse-9659a.firebaseapp.com",
-    databaseURL: "https://warehouse-9659a-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "warehouse-9659a",
-    storageBucket: "warehouse-9659a.appspot.com",
-    messagingSenderId: "1008749761087",
-    appId: "1:1008749761087:web:18639ef5eef766d40d9e8a",
-    measurementId: "G-3Y9RWX8TF1"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
 
 // Initialize Firebase
